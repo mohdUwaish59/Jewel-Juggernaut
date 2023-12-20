a4eaf0194eb9eb5470d50eddd7358f34e2a0f63f
mohdUwaish59 <mohd.uwaish.e21@nsut.ac.in>
2023-12-20 22:20:54 +0100

    Changes made in Models, and app.py

--- app1.py
+++ app1.py
@@ -33,16 +33,25 @@ def predict_price(prediction_pipeline, carat, cut, color, clarity, depth, table,
 def main():
     st.title("Diamond Price Prediction App")
 
-    # Input fields
-    carat = st.number_input("Carat", min_value=0.2, max_value=5.0, step=0.1, value=1.0)
-    cut = st.selectbox("Cut", ["Fair", "Good", "Very Good", "Premium", "Ideal"])
-    color = st.selectbox("Color", ["D", "E", "F", "G", "H", "I", "J"])
-    clarity = st.selectbox("Clarity", ["I1", "SI2", "SI1", "VS2", "VS1", "VVS2", "VVS1", "IF"])
-    depth = st.number_input("Depth (%)", min_value=40, max_value=80, step=1, value=60)
-    table = st.number_input("Table (%)", min_value=50, max_value=80, step=1, value=60)
-    x = st.number_input(label="X", step=1., format="%.2f")
-    y = st.number_input(label="Y", step=1., format="%.2f")
-    z = st.number_input(label="Z", step=1., format="%.2f")
+    col1, col2, col3 = st.columns(3)
+
+    # Column 1
+    with col1:
+        carat = st.number_input("Carat", min_value=0.2, max_value=5.0, step=0.1, value=1.0)
+        cut = st.selectbox("Cut", ["Fair", "Good", "Very Good", "Premium", "Ideal"])
+        color = st.selectbox("Color", ["D", "E", "F", "G", "H", "I", "J"])
+
+    # Column 2
+    with col2:
+        clarity = st.selectbox("Clarity", ["I1", "SI2", "SI1", "VS2", "VS1", "VVS2", "VVS1", "IF"])
+        depth = st.number_input("Depth (%)", min_value=40, max_value=80, step=1, value=60)
+        table = st.number_input("Table (%)", min_value=50, max_value=80, step=1, value=60)
+
+    # Column 3
+    with col3:
+        x = st.number_input(label="X", step=1., format="%.2f")
+        y = st.number_input(label="Y", step=1., format="%.2f")
+        z = st.number_input(label="Z", step=1., format="%.2f")
 
     # Button to make predictions
     if st.button("Predict Price"):
